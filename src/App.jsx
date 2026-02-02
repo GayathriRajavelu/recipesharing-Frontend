2437dfa61074670e552d9877e9468a3fc07e1446
GayathriRajavelu <gayathrirajavelu99@gmail.com>
2026-02-02 08:04:14 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
